1b2ae1308d80d4a6794cbb45b75d9872ffe8d1b7
David Pérez <dperezsache.guadalupe@alumnado.fundacionloyola.net>
2023-05-30 16:20:15 +0200

    Datos de prueba funcionando?

--- sql/scriptdatosprueba.sql
+++ sql/scriptdatosprueba.sql
@@ -1,2 +1,14 @@
 INSERT INTO `Persona` (`nombre`, `apellidos`, `correo`, `clave`, `telefono`, `dni`, `iban`, `titular`)
-VALUES ('Bob', 'Esponja', 'email@gmail.com', '$2y$15$.LtfOiAtM44kRXnPP3AbQODd00CdEWL0/dwcZwmj890ebBFXo0LG6', '609040501', '82307805R', 'ES9420805801101234567891', 'Bob');
+VALUES ('David', 'Pérez', 'email@gmail.com', '$2y$15$.LtfOiAtM44kRXnPP3AbQODd00CdEWL0/dwcZwmj890ebBFXo0LG6', '609040501', '82307805R', 'ES9420805801101234567891', 'David');
+
+INSERT INTO `Padre` (`id`) 
+VALUES (1);
+
+INSERT INTO `Persona` (`nombre`, `apellidos`)
+VALUES ('Patricio', 'Estrella');
+
+INSERT INTO `Hijo` (`id`, `idCurso`)
+VALUES (2, 5);
+
+INSERT INTO `Hijo_Padre` (`idPadre`, `idHijo`)
+VALUES (1, 2);
